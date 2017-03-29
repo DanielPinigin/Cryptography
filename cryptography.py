fb86ba77638f599e338eea00849af5a96e2a9ef0
DanielPinigin <danielpinigin@hanovernorwichschools.org>
2017-03-29 13:58:14 -0400

    Updated from Brython Server: 3/29/2017 1:58:14 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Daniel
+Credit: 
 
 Assignment:
 
@@ -10,3 +10,18 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+#"
+#Enter e to encrypt, d to decrypt, or q to quit: z
+#Did not understand command, try again.
+#Enter e to encrypt, d to decrypt, or q to quit: e
+#Message: Two plus two = Five
+#Key: Lorem ipsum
+#+KF;B(CH=NIZ}m;R\Dt
+#Enter e to encrypt, d to decrypt, or q to quit: d
+#Message: +KF;B(CH=NIZ}m;R\Dt
+#Key: Lorem ipsum
+#Two plus two = Five
+#Enter e to encrypt, d to decrypt, or q to quit: q
+#Goodbye!
+"
